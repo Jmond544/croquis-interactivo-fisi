d7aa89a2a79a5ce20e8a92f8ae47e5a399e9b0e4
Jmond544 <jmondalgotapia@gmail.com>
2023-08-01 16:08:18 -0500

    feat: :art: Trying more posibiliies

--- src/components/Model3d.jsx
+++ src/components/Model3d.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "../style-sheets/Model3d.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 function Model3d() {
   const mountRef = useRef(null);
-
+  const buttonResetRef = useRef(null);
+  const [initialCameraPosition, setInitialCameraPosition] = useState({ x: 6, y: 4, z: 6 });
+  
   useEffect(() => {
     const currentRef = mountRef.current;
     const { clientWidth: width, clientHeight: height } = currentRef;
-
+    
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(25, width / height, 0.1, 1000);
-    camera.position.z = 6;
-    camera.position.x = 6;
+    camera.position.set(initialCameraPosition.x, initialCameraPosition.y, initialCameraPosition.z);
     
+    // Restablecer la posición de la cámara a la posición inicial almacenada en el estado
+
     scene.background = new THREE.Color(0xbde0fe);
 
     const renderer = new THREE.WebGLRenderer();
@@ -24,30 +27,39 @@ function Model3d() {
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
+    controls.enableRotate = false;
+    controls.dampingFactor = 0.04;
+    controls.minDistance = 1;
+    controls.maxDistance = 15;
 
     /* Cubos */
     const geometry = new THREE.BoxGeometry(6, 0.1, 3);
-    const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff04 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
     const cube2 = cube.clone();
-    cube2.position.y = 1;
+    cube2.position.y = 2;
     scene.add(cube2);
 
     const cube3 = cube.clone();
-    cube3.position.y = -1;
+    cube3.position.y = -2;
     scene.add(cube3);
+
+    const cube4 = cube.clone();
+    cube4.position.y = -2;
+    cube4.geometry = new THREE.BoxGeometry(1, 0.5, 1);
+    scene.add(cube4);
     
-    camera.lookAt(cube.position);
 
     /* Linea */
 
-    const material2 = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    const material2 = new THREE.LineBasicMaterial( { color: 0x0000ff, linewidth: 10 });
     const points = [];
     points.push( new THREE.Vector3( - 2, 0, 0 ) );
     points.push( new THREE.Vector3( 0, 2, 0 ) );
     points.push( new THREE.Vector3( 2, 0, 0 ) );
+    points.push( new THREE.Vector3( 2, 2, 0 ) );
 
     const geometry2 = new THREE.BufferGeometry().setFromPoints( points );
 
@@ -72,14 +84,27 @@ function Model3d() {
 
     animate();
 
+    const handleResetClick = () => {
+      camera.position.set(initialCameraPosition.x, initialCameraPosition.y, initialCameraPosition.z);
+      controls.update();
+    }
+
+    buttonResetRef.current?.addEventListener('click', handleResetClick);
+
     renderer.render(scene, camera);
 
     return () => {
       currentRef.removeChild(renderer.domElement);
     };
-  }, []);
+  }, [initialCameraPosition.x, initialCameraPosition.y, initialCameraPosition.z]);
 
-  return <div ref={mountRef} className="container"></div>;
+  return (
+  <div className="container-principal">
+    <div ref={mountRef} className="container-3d"></div>
+    <button ref={buttonResetRef} >Reset position</button>
+
+  </div>
+  );
 }
 
 export default Model3d;
